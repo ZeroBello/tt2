c674912e0b2b8eeba751226315f422ce2f40827e
Le Duc Anh <anhldgch17168@fpt.edu.vn>
2019-08-31 18:13:36 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,14 +5,14 @@
             <a href="">Manage Catalogue</a>
             <ul>
                 <li><a href="addcatalogue.php">Add Catalogue</a></li>
-                <li><a href="managecatalogue.php">Delete catalogue</a></li>
+                <li><a href="managecatalogue.php">View/Search/Update/Delete catalogue</a></li>
             </ul>
         </li>
         <li>
             <a href="">Manage Items</a>
             <ul>
                 <li><a href="additem.php">Add Item</a></li>
-                <li><a href="updateitem.php">View/Search/Update/Delete item</a></li>
+                <li><a href="manageitem.php">View/Search/Update/Delete item</a></li>
             </ul>
         </li>
     </ul>
