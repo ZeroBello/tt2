49d1263ad66dcf326a9b9e1fcf0acf79cfdc52c7
Le Duc Anh <anhldgch17168@fpt.edu.vn>
2019-08-31 18:26:47 +0700

    Update index-product.php

--- index-product.php
+++ index-product.php
@@ -44,9 +44,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
                 {    
                     echo "<tr>";
-                        for($i=1;$i<=5;$i++)
+                        for($i=1;$i<=7;$i++)
                         {
-                            echo "<td width='78px'>";
+                            echo "<td width='100px'>";
                                 if($row!=false)
                                 {
                                     $ID = $row ['id'];
@@ -86,7 +86,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
             {    
                 echo "<tr>";
-                    for($i=1;$i<=5;$i++)
+                    for($i=1;$i<=3;$i++)
                     {
                         echo "<td align='center' width='500px' height='200px' >";
                             if($row!=false)
