68205d7328349893ab2c0e8ca2f9f6c7f1506a1f
Le Duc Anh <anhldgch17168@fpt.edu.vn>
2019-08-31 17:30:06 +0700

    jj

--- updatecatalogue.php
+++ updatecatalogue.php
@@ -2,19 +2,19 @@
     include("database.php");    
 ?>
 <?php	
-	if( isset($_POST['cid'], $_POST['cname']))
+	if( isset($_POST['id'], $_POST['cname']))
 	{
 	    
-	    $sql = "UPDATE Catalogue SET cname = :cname WHERE cid = :cid";
+	    $sql = "UPDATE Catalogue SET cname = :cname WHERE id = :id";
 	    $stmt= $pdo->prepare($sql);
-	    $stmt->bindValue(':cid', $_POST['cid'], PDO::PARAM_INT);
+	    $stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
 	    $stmt->bindValue(':cname', $_POST['cname'], PDO::PARAM_STR);
 	    $pdoExec = $stmt->execute();
 	    
 	        // check 
 	    if($pdoExec)
 	    {
-	        die("You've updated the Catalogue '$cid' <a href='managecatalogue.php'>click here</a> to continue.");
+	        die("You've updated the Catalogue '$id' <a href='managecatalogue.php'>click here</a> to continue.");
 	    }else{
 	        echo 'Data Not updated';
 	    }
