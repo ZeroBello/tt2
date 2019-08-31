a449eae507656ad42ffaf645804c85e1925e4456
Le Duc Anh <anhldgch17168@fpt.edu.vn>
2019-08-31 16:01:11 +0700

    jj

--- deletecatalogue.php
+++ deletecatalogue.php
@@ -1,29 +1,13 @@
-<?php
-session_start();
-require_once './database.php';
-if (isset($_POST['id'])) {
-    $cId = sanitizeString($_POST['id']);
-    $query = "DELETE FROM Catalogue WHERE id = '$id'";
-    queryMysql($query);
-    //header("Location: loadcatalogue.php");
-    die("You've deleted the catalogue '$id' <a href='loadcatalogue.php'>click here</a> to continue.");
+<?php 
+    include("database.php");
+?>
+<?php	
+	if( isset($_POST['id']) ){
+    
+    $sql = "DELETE FROM Catalogue WHERE id = :id";
+    $stmt= $pdo->prepare($sql);
+    $stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+    $stmt->execute();
+    die("You've deleted the CCatalogue '$cid' <a href='managecatalogue.php'>click here</a> to continue.");
 }
 ?>
-<?php
-    while ($row = mysqli_fetch_array($result)) {
-        $cname = $row[1];
-        echo "<tr>";
-        echo "<td>$cname</td>";
-      
-        ?>
-        <td>
-            <form class="frminline" action="deletecatalogue.php" method="post" onsubmit="return confirmDelete();">
-                <input type="hidden" name="cId" value="<?php echo $row[0] ?>" />
-                <input type="submit" value="Delete" />
-            </form>
-                 </td>
-        <?php
-        echo "</tr>";
-    }
-    ?>
-    
