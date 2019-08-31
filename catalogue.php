65f1336da9409f2601d288e79d1ad3127807aaf7
Le Duc Anh <anhldgch17168@fpt.edu.vn>
2019-08-31 17:33:59 +0700

    Update catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -4,7 +4,8 @@
     $sql = "SELECT id, cname from Catalogue";
         $stmt = $pdo->prepare($sql);
         //Thiết lập kiểu dữ liệu trả về
-        //$stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $stmt->execute();
+        $resultSet = $stmt->fetchAll();
     
 ?>
